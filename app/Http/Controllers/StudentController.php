fe710e09a65f7f0faa46f974ea916f0760eb7bc3
Sean Skinner <sean@theskinners.id.au>
2026-04-20 17:07:47 +1000

    idk

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
     public function show($id)
     {
-        $student = DB::table('students')->where('id', $id)->first();
-
-        if (!$student) {
-            abort(404);
+        if (!auth()->check()) {
+            abort(403);
         }
+
+        // TODO: When multi-school support is introduced, restrict access by school_id
+        $student = Student::where('id', $id)->firstOrFail();
 
         return view('students.show', compact('student'));
     }
